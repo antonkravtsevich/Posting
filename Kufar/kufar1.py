414bf83cc07a5ee7e33fa1f3bd36dc8255d72d52
inn9 <antonkravtsevich@gmail.com>
2017-02-23 22:29:56 +0300

    Comments added

--- Kufar/kufar1.py
+++ Kufar/kufar1.py
@@ -5,16 +5,50 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+# крч по поводу того, что за ересь этот db.json
+# все данные для выкладки можно представить в формате
+# название, имя на русском, тип, xpath.
+# чтобы не прописывать всю эту херню и добавить немного динамики, я вынес большую часть всей той херни с данными в
+# json документ, и для выкладки подтягиваю их оттуда. Таким образом проге по сути похер откуда тянуть данные, она
+# их тупо выкладывает. Вооооот.
+# БД имеет следующую структуру:
+# в элементе common хранится инфа обо всех общих данных каждого товара. Всякие там названия, описания, состояние и
+# прочая херь.
+# потом идет элемент not_common
+# В not_common хранятся поименованные элементы, отвечающие за данные, специализированные для каждой конкретной
+# категории. Типа размер для обуви, диагональ для телика и прочая херня. Плюс все это сдобрено кучей xpath-ов
+# и всяких там наименований. Соответственно, вообще все данные, необходимые для выкладки товара определенной
+# категории, будут являтся сумммой всех данных из common и данных из элемента not_common, имя которого совпадает
+# с названием категории. То бишь крч все круто и няняня, но если еще начнет работать - будет в два раза круче.
+
+# А, да, чуть не забыл. Мобильники выкладываются хорошо, вообще прям без проблем, ммммм как здорово, но как только
+# пытаюсь выложить что-то из другой категории - вылетает на первом же элементе из not_common. На марке. И я хз
+# че делать. Вот прям вообще хз хз хз.
+
+
+# выбор пункта в ниспадающем списке
+# browser - драйвер, xpath_click - xpath, который нужно кликнуть для того, чтобы ниспадающий список ниспал,
+# xpath_ui - общий адрес ui, содержащий список li-шных пунктов меню, string - строка, пункт с коротой нужно
+# выбрать. Ну типа из списка меню выбрать марку - тогда в string передается именно марка.
 def select_in_select(browser, xpath_click, xpath_ui, string):
     try:
+        # поиск элемента, по которому нужно кликнуть, чтобы появился список
+        # с ожиданием!
         element = WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.XPATH, xpath_click))
         )
         element.click()
+
+        # поиск ui с пунктами меню
         ui = WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.XPATH, xpath_ui))
         )
+
+        # выборка всех пунктов меню (тэг li) из ui
         li_s = ui.find_elements_by_tag_name("li")
+
+        # поиск элемента, текст которого соответствует требуемому.
+        # (крч поиск правильного элемента)
         right_elem = li_s[0]
         for li in li_s:
             div = li.find_element_by_tag_name("div")
@@ -27,6 +61,7 @@ def select_in_select(browser, xpath_click, xpath_ui, string):
         i = 1
 
 
+# функция входа в аккаунт
 def login(username, password, browser):
     browser.find_element_by_xpath("//a[@id='add']").click()
     browser.find_element_by_xpath("//fieldset[@class='form_login_fieldset form_login_fieldset__email']/input[@id='email']").send_keys(username)
@@ -35,6 +70,9 @@ def login(username, password, browser):
     return browser
 
 
+# функция выкладки. browser - драйвер, all-data - выборка данных, необходимых для выкладки
+# (общие для всех категорий данные + данные для определенной категории)
+# вызывает отдельные методы для выкладки различных элементов (лежат ниже)
 def posting(browser, all_data):
     for data in all_data:
         print(data['rus_name'])
@@ -46,6 +84,7 @@ def posting(browser, all_data):
             show_input(browser, data)
 
 
+# ввод данных в ниспадающий список
 def show_select(browser, data):
     print(data['values'])
     input_data = str(input())
@@ -55,6 +94,7 @@ def show_select(browser, data):
     select_in_select(browser, xpath, xpath_ui, input_data)
 
 
+# вывод данных в чекбокс
 def show_checkbox(browser, data):
     print("y/n:")
     input_data = str(input())
@@ -64,6 +104,7 @@ def show_checkbox(browser, data):
         checkbox.cick()
 
 
+# ввод данных в инпут
 def show_input(browser, data):
     print("Enter data:")
     input_data = str(input())
@@ -72,6 +113,7 @@ def show_input(browser, data):
     input_elem.send_keys(input_data)
 
 
+# выборка данных из db.json. Набираются общие для всех категорий данные + данные выбранной категории
 def get_posting_data(db, category_name):
     common = db["common"]
     not_common = db["not_common"]
@@ -85,20 +127,30 @@ def get_posting_data(db, category_name):
     return all_data
 
 
+# метод для перехода к нужной категории товара
 def choose_category(category_name, db, browser):
+    # получение информации о выбранной категории. xpath-ы, необходимые для выкладки объявления данные
     not_common = db['not_common']
     for elem in not_common:
         if (elem['category_name'] == category_name):
             category = elem
     category = category['category']
+    # подборка селекторов для перехода к нужной категории.
+    # селектор0 - для открытия списка категорий
     selector0 = "//div[@class='ai_category']/b[@class='ai_category__label']"
+    # селектор1 - выбирает пункт в основном меню
     selector1 = category['category_selector1']
+    # селектор2 = выбирает пункт во вторичном меню
     selector2 = category['category_selector2']
+    # а тут крч пошел выбор данных
     try:
+        # ожидание подгрузки элемента, который открывает ниспадающий список
         element = WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.XPATH, selector0))
         )
+        # открываем список
         element.click()
+        # выбор нужных элементов меню
         browser.find_element_by_xpath(selector1).click()
         element = WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, selector2))
@@ -108,6 +160,7 @@ def choose_category(category_name, db, browser):
         if(category['category_select']!='none'):
             select_in_select(browser, category['category_select'], category['category_ui'], category_name)
 
+        # выбор пункта выкладки "без продвижения"
         checkbox = browser.find_element_by_xpath("//label[@class='package_none_label']/b")
         checkbox.click()
     finally:
@@ -116,24 +169,38 @@ def choose_category(category_name, db, browser):
 
 
 def main():
+    # установка опций браузера (открытие в полном размере окна)
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
+    # если юзаешь другой браузер - закомменть две предыдущие строки
+    # но не советую, потому что в мозиле не работает клик по li элементу, и соответственно моя функция
+    # select_in_select. Такие дела.
     browser = webdriver.Chrome(chrome_options=options)
+    # открытие и обработка файла db.json
     db_file = open('./db.json', 'r')
     db_source = db_file.read()
     db = json.loads(db_source)
-    print("Выберите категорию: Планшеты и электронные книги, Мобильные телефоны, Телевизоры, Ноутбуки, Компьютеры")
+    print("Выберите категорию: ")
+    print("Планшеты и электронные книги, Мобильные телефоны, Телевизоры, Ноутбуки, Компьютеры / системные блоки")
     category_name = str(input())
     data = get_posting_data(db, category_name)
+
+    # начало выкладки
     start_url = "https://www.kufar.by/%D0%B1%D0%B5%D0%BB%D0%B0%D1%80%D1%83%D1%81%D1%8C"
     username = "user120895@gmail.com"
     password = "siniza314"
     browser.get(start_url)
-    browser.save_screenshot('./screen1.png')
-    # browser.find_element_by_xpath("//a[@class='joyride-close-tip']").click()
+
+    # вход в аккаунт
     browser = login(username, password, browser)
+
+    # выбор категории
     choose_category(category_name, db, browser)
+
+    # выкладка данных
     posting(browser, data)
+
+    # ожидание (позырить, че как где произошло)
     print("wait")
     wait = input()
 
